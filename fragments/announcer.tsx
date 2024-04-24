bfcb4cd35059e3cbe0c729ffae3bcb53f779d315
Yusril Muttaqien <idyusril@gmail.com>
2024-04-24 13:55:26 +0700

    chore - announcer interactive tidyup

--- fragments/announcer.tsx
+++ fragments/announcer.tsx
@@ -1,26 +1,9 @@
-import { useEffect } from 'react';
-import { motion, useAnimationControls } from 'framer-motion';
-import { useAnimationSequenceCtx } from '@/providers/animation-sequence';
+import { motion } from 'framer-motion';
+import useAnnouncerInteractive from '@/hooks/announcer-interactive';
 import classMerge from '@/utils/class-merge';
 
 export default function Announcer() {
-  const control = useAnimationControls();
-  const {
-    state: {
-      announcer: { announcing },
-    },
-  } = useAnimationSequenceCtx();
-
-  useEffect(() => {
-    if (announcing) {
-      control.start(
-        { scale: 1 },
-        { repeat: Infinity, repeatType: 'reverse', duration: 1.5, ease: 'easeInOut' }
-      );
-    } else {
-      control.start({ scale: 0 }, { duration: 1.5, ease: 'easeInOut' });
-    }
-  }, [announcing, control]);
+  const control = useAnnouncerInteractive();
 
   return (
     <motion.div
---
+++ hooks/announcer-interactive.ts
@@ -0,0 +1,25 @@
+import { useAnimationControls } from 'framer-motion';
+import { useAnimationSequenceCtx } from '@/providers/animation-sequence';
+import useIsomorphicLayoutEffect from '@/hooks/isometric-effect';
+
+export default function useAnnouncerInteractive() {
+  const control = useAnimationControls();
+  const {
+    state: {
+      announcer: { announcing },
+    },
+  } = useAnimationSequenceCtx();
+
+  useIsomorphicLayoutEffect(() => {
+    if (announcing) {
+      control.start(
+        { scale: 1 },
+        { repeat: Infinity, repeatType: 'reverse', duration: 1.5, ease: 'easeInOut' }
+      );
+    } else {
+      control.start({ scale: 0 }, { duration: 1.5, ease: 'easeInOut' });
+    }
+  }, [announcing, control]);
+
+  return control;
+}
