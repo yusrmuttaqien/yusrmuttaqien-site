439da76b0d07e8d567a4d3d25b12687c84707e79
Yusril Muttaqien <idyusril@gmail.com>
2024-06-07 13:49:15 +0700

    update - Image component type

--- src/components/Image/type.ts
+++ src/components/Image/type.ts
@@ -1,9 +1,10 @@
 import { IMAGE_STYLES } from '@/components/Image';
+import type { StaticImageData } from 'next/image';
 
 export type ImageProps = {
   className?: Partial<typeof IMAGE_STYLES.slots>;
   scale?: number;
-  src: string;
+  src: string | StaticImageData;
   alt: string;
 };
 export type InteractiveParams = {
